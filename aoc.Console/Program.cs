6e233270881e405c664f42b03d06c25da5daf022
Michael Zink <michael.zink@iits-consulting.de>
2023-12-15 10:50:37 +0100

    day 14 - Parabolic Reflector Dish

--- aoc.Console/Program.cs
+++ aoc.Console/Program.cs
@@ -4,7 +4,9 @@ using aoc.Console.Puzzles;
 
 //var calibrationValue = Trebuchet.GetCalibrationValue();
 //Console.WriteLine($"Calibration value for day #1 is {calibrationValue}");
-var gameIdSum = CubeConundrum.GetIdSumResult(null, 12, 14, 13);
-Console.WriteLine($"Sum of valid game ids is {gameIdSum}");
-var powers = CubeConundrum.GetPowers();
-Console.WriteLine($"Sum of powers is {powers}");
+//var gameIdSum = CubeConundrum.GetIdSumResult(null, 12, 14, 13);
+//Console.WriteLine($"Sum of valid game ids is {gameIdSum}");
+//var powers = CubeConundrum.GetPowers();
+//Console.WriteLine($"Sum of powers is {powers}");
+var dishWeight = ParabolicReflectorDish.GetTotalWeight(1000000000);
+Console.WriteLine($"Total stress on northern support beams is {dishWeight}");
---
+++ aoc.Tests/Puzzles/14_ParabolicReflectorDishTests.cs
@@ -0,0 +1,86 @@
+﻿using aoc.Console.Puzzles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc.Tests.Puzzles
+{
+    public class ParabolicReflectorDishTests
+    {
+        private const string dish = @"O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+
+        [Fact]
+        public void TiltLeverShouldMoveAllRocksNorthAsFarAsPossible()
+        {
+            var expected = @"OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....";
+
+            var result = string.Join(Environment.NewLine, ParabolicReflectorDish
+                .TiltLever(dish.Split(Environment.NewLine)));
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("O.OO#....#", ".....##...", "O.OO#....#", ".....##...")]
+        [InlineData("..#....#.#", "..O..#.O.O", "..#....#.#", "..O..#.O.O")]
+        [InlineData(".....##...", "OO.#O....O", "OO..O##..O", "...#......")]
+        public void MoveNorthShouldSwapRoundRocksIntoNorthernRowIfSpaceIsAvailable(string north, string south, string expectedNorth, string expectedSouth)
+        {
+            var result = ParabolicReflectorDish.MoveNorth(north, south);
+
+            Assert.Equal(expectedNorth, result[0]);
+            Assert.Equal(expectedSouth, result[1]);
+        }
+
+        [Theory]
+        [InlineData("OOOOOOOO", 10, 80)]
+        [InlineData("OOOOOOOO", 1, 8)]
+        [InlineData("........", 10, 0)]
+        [InlineData("O.OO#....#", 5, 15)]
+        public void CalculateWeigthShouldSumRoundRocksAndApplyWeightFactor(string line, int weight, int expected)
+        {
+            var result = ParabolicReflectorDish.CalculateRowWeight(line, weight);
+            
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetTotalWeightShouldCalcluateSumOfRowWeights()
+        {
+            var result = ParabolicReflectorDish.GetTotalWeight(0, dish);
+            Assert.Equal(136, result);
+        }
+
+        [Theory]
+        [InlineData(1, ".....#....\r\n....#...O#\r\n...OO##...\r\n.OO#......\r\n.....OOO#.\r\n.O#...O#.#\r\n....O#....\r\n......OOOO\r\n#...O###..\r\n#..OO#....")]
+        [InlineData(2, ".....#....\r\n....#...O#\r\n.....##...\r\n..O#......\r\n.....OOO#.\r\n.O#...O#.#\r\n....O#...O\r\n.......OOO\r\n#..OO###..\r\n#.OOO#...O")]
+        [InlineData(3, ".....#....\r\n....#...O#\r\n.....##...\r\n..O#......\r\n.....OOO#.\r\n.O#...O#.#\r\n....O#...O\r\n.......OOO\r\n#...O###.O\r\n#.OOO#...O")]
+        public void CycleShouldTurnInputDataAround(int numberOfCycles, string expectedResult)
+        {
+            var split = dish.Split("\r\n");
+            var result = ParabolicReflectorDish.Cycle(split, numberOfCycles);
+
+            Assert.Equal(expectedResult, string.Join("\r\n", result));
+        }
+    }
+}
